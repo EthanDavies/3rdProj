5be3cf479ddbc1d12ac00f8d3d5b5ad3f5148f28
TheGreatEthan <ethandavies15@gmail.com>
2017-04-10 19:10:50 +0100

    Spoken word option added to posts

--- FinalYearProject/Global.asax.cs
+++ FinalYearProject/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
 
 namespace FinalYearProject
 {
@@ -16,6 +19,24 @@ namespace FinalYearProject
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //check for bin files to be loaded
+            CheckAddBinPath();
+        }
+
+        public static void CheckAddBinPath()
+        {
+            // find path to 'bin' folder
+            var binPath = Path.Combine(new string[] { AppDomain.CurrentDomain.BaseDirectory, "bin" });
+            // get current search path from environment
+            var path = Environment.GetEnvironmentVariable("PATH") ?? "";
+
+            // add 'bin' folder to search path if not already present
+            if (!path.Split(Path.PathSeparator).Contains(binPath, StringComparer.CurrentCultureIgnoreCase))
+            {
+                path = string.Join(Path.PathSeparator.ToString(CultureInfo.InvariantCulture), new string[] { path, binPath });
+                Environment.SetEnvironmentVariable("PATH", path);
+            }
         }
     }
 }
